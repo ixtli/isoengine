388e903ad167078b9c7b016dcd057fe318a59530
Chris Galardi <ixplode@gmail.com>
2013-01-26 16:50:30 -0500

    Initial Commit

---
+++ isoEngine/isoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  isoAppDelegate.h
+//  isoEngine
+//
+//  Created by Ixtli on 1/26/13.
+//  Copyright (c) 2013 isoCo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface isoAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ isoEngine/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  isoEngine
+//
+//  Created by Ixtli on 1/26/13.
+//  Copyright (c) 2013 isoCo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
